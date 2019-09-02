56565326dcc72ead4c1c2dd19c172490485fb608
jally-ph <46050484+jally-ph@users.noreply.github.com>
2019-09-02 12:53:28 +0200

    dev _ define
    au lien de CONST mettre Define()

---
+++ dev.php
@@ -0,0 +1,10 @@
+<?php
+//Nos constantes
+
+define('HOST', 'localhost'); 
+define('DB_NAME', 'projet4.1'); 
+define('CHARSET', 'utf8'); 
+define('DB_HOST', 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET);
+define('DB_USER', 'root');
+define('DB_PASS', '');  
+
